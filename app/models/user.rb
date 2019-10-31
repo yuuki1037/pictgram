8eead3a8299b9f886d819fe45d22edeeb68b5745
EC2 Default User <ec2-user@ip-172-31-42-201.us-east-2.compute.internal>
2019-10-31 04:44:30 +0000

    ユーザー登録

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  validates :name, presence: true
+  validates :name, length: { maximum: 15 }
+  validates :email, presence: true
+  MA_REGEX = /[a-zA-Z0-9._+]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*/
+  varidates :email, presence: true, format: {with: MA_REGEX }
+   has_secure_password
+   validates :password,length: { in: 8..32 }
+PW_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\z/i
+  validates :password, presence: true, format: { with: PW_REGEX }
+
+end
